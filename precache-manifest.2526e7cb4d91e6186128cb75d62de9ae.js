419eebd0122a0a7513e9454ba56ed6c534944a22
pradeepnedun@gmail.com <ajram1998@gmail.com>
2020-09-04 15:35:11 +0530

    build

---
+++ precache-manifest.2526e7cb4d91e6186128cb75d62de9ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72dd0197bf13ac449b2a2716fbd8aef4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ae847ef7224648962be",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0df29d7379d749561fa1",
+    "url": "/static/css/main.106b0a5f.chunk.css"
+  },
+  {
+    "revision": "8ae847ef7224648962be",
+    "url": "/static/js/2.ac2bbb2a.chunk.js"
+  },
+  {
+    "revision": "0df29d7379d749561fa1",
+    "url": "/static/js/main.8185a97e.chunk.js"
+  },
+  {
+    "revision": "053ed70e1f0f667a8995",
+    "url": "/static/js/runtime-main.bbf3836b.js"
+  },
+  {
+    "revision": "51a3f4ee1148645ebd7b67a099c88e41",
+    "url": "/static/media/avatar-1.51a3f4ee.PNG"
+  },
+  {
+    "revision": "93eba42b4e74374701cc1dec028587e2",
+    "url": "/static/media/avatar-2.93eba42b.PNG"
+  },
+  {
+    "revision": "ad5085f46ecd36c242d07a51151bb78a",
+    "url": "/static/media/avatar-3.ad5085f4.PNG"
+  }
+]);
